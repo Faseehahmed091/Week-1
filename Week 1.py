10764c0c1ee61a849b9fd2d72c08fd38fe5ef503
Faseehahmed091 <146334115+Faseehahmed091@users.noreply.github.com>
2023-11-15 21:35:09 +0000

    Add files via upload

---
+++ Week 1.py
@@ -0,0 +1,9 @@
+print('the program has executed')
+print('We starting python programming')
+print('Our test is in week 9')
+print('I have recalled the commands')
+help('keywords')
+help('print')
+print('Hello, World')
+
+
